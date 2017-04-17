68af50d11b585eaf4f9cd5341856969568c698d4
Sergey Zhmylove <zhmylove@narod.ru>
2017-04-17 19:30:39 +0300

    Add first perl code file with perlpod skeleton

---
+++ vilan.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use strict;
+use v5.18;
+use warnings;
+no warnings 'experimental';
+use utf8;
+binmode STDOUT, ':utf8';
+
+=encoding utf-8
+
+=head1 NAME
+
+B<vilan> -- LAN topology visualizer
+
+=head1 SYNOPSIS
+
+   vilan [ -c config.pm ]
+
+Here will be some examples of syntax and all parameters
+
+=head1 DESCRIPTION
+
+The B<vilan> utility scans network and makes it's topology in digraph format.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=cut
+
+=item f()
+   piu piu
+
+=cut
+
+#code
+
+=back
+
+=head1 DEPENDENCIES
+
+#TODO
+   nmap >= 5.0
+
+=head1 AUTHORS
+
+Originally developed by Lev Koznov and Sergey Zhmylove.
+This program is free software distributed AS-IS. 
+You can redistribute it and/or modify it under terms of a licence
+bundled within this package.
+Copyright, 2017.
+
+=cut
